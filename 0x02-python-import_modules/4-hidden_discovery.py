a3ad0ca1efc463aadf1e116e3ca4d75f62884dad
Emmaobi7 <emma46574@gmail.com>
2022-12-02 13:02:34 +0000

    python modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-
-dir(hidden_4.pyc)
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
