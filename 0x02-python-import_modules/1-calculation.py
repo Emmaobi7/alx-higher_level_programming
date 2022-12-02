d586715d748d85dc8d8e82590a8a335807f0e808
Emmaobi7 <emma46574@gmail.com>
2022-12-02 21:58:38 +0100

    python modulr task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import calculator_1 as func
-
 if __name__ == "__main__":
-    a = 10
-    b = 5
+    from calculator_1 import add, sub, mul, div
 
-    print("{:d} + {:d} = {:d}".format(a, b, func.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, func.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, func.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, func.div(a, b)))
+
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
