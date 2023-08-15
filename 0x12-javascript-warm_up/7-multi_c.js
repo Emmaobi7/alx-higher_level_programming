803e6523fda4b56b2a5d127710071eecf8c3dc14
root <Emmaobi7>
2023-08-15 00:01:28 +0000

    added code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,12 @@
 
 const myVar = process.argv;
 const check = parseInt(myVar[2], 10);
-let index;
+let index = 0;
 
 if (!isNaN(check)) {
   while (index < check) {
     console.log('C is fun');
+    index++;
   }
 } else {
   console.log('Missing number of occurrences');
