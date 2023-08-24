9ac2a3e251a15ef5381803175e72fabe70891196
Emmaobi7 <emma46574@gmail.com>
2023-08-24 10:20:57 -0700

    test

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""creates a state db"""
+
+from sqlalchemy import Column, create_engine, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a table states in mysql
+    state: the table named states
+    (Base): parent class where states inherits from
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
