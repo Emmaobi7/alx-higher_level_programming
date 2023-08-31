497bdc97c34408283acf26c7f7b394b4ee46c13e
Emmaobi7 <emma46574@gmail.com>
2023-08-31 12:11:08 -0700

    test

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,12 @@
+#1/usr/bin/python3
+""" a script to find the
+peak in an unsorted list
+"""
 def find_peak(list_of_integers):
+    """
+    find_peak: function to find peak
+    (list_of_integers): a list of int
+    """
     if list_of_integers:
         return max(list_of_integers)
     else:
