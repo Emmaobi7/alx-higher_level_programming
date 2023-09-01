5fe7d1c6b6d8f34f7b66213f8f7371ae0aec387a
Emmaobi7 <emma46574@gmail.com>
2023-09-02 00:42:42 +0100

    tesr

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,7 @@ if __name__ == '__main__':
     owner = sys.argv[2]
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     res = requests.get(url)
-    commit = res.json()
+    commits = res.json()
 
-    for index in range(10):
-        sha = commit[index].get('sha')
-        author = commit[index].get('commit').get('author').get('name')
-        print(f'{sha}: {author}')
+    for commit in commits[0:10]:
+        print(commit['sha'] + ':', commit['commit']['author']['name'])
