cbda8f50b1d9b273ff1fa61ba81c6a9b87cd544b
Emmaobi7 <emma46574@gmail.com>
2023-09-01 08:30:54 -0700

    t

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """fethches https:...."""
 
-import urllib.request
-
 
 if __name__ == '__main__':
+    import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         html = resp.read()
         print('Body response:')
